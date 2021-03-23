984d46653c0e1d7364fd37a9078d8627ca29a768
apeden <apeden23@gmail.com>
2021-03-23 15:16:30 +0000

    Now making a plan of CDI plates in pandas dataframes, need to find a way of keeping flexibility though

--- experiment.py
+++ experiment.py
@@ -2,6 +2,7 @@ import math
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+pd.set_option('display.max_rows', None)
 
 FFI = {"44/2019(MV)":"FC",
        "82/2017(MM)":"CNS",
@@ -31,11 +32,21 @@ class Plate(Material):
                 "COLUMN": pd.Series([(x//8)+1 for x in range(1,97)]),
                 "WELL": pd.Series(list(range(1,97)), dtype="int32"),
                 "ug per well recPrP": 0.0,
-                "VALUE": 0.0,
             }
         )
         self.triblock = 1
     def addAnalyte(self, compon):
+        tissue, conc = compon
+        point = 1
+        point += (self.triblock-1)*4
+        self.comp.loc[point: (point+2), "Denatured"] = False
+        self.comp.loc[point: (point+2), "Sample"] = tissue.getName()
+        self.comp.loc[point: (point+2), "PK"] = conc
+        self.comp.loc[point+3: (point+5), "Denatured"] = True
+        self.comp.loc[point+3: (point+5), "Sample"] = tissue.getName()
+        self.comp.loc[point+3: (point+5), "PK"] = conc
+        self.triblock += 2
+    def addCalibrant(self, compon):
         point = 1
         if self.triblock%2 != 0:
             point += (self.triblock-1)*4
@@ -44,13 +55,11 @@ class Plate(Material):
         self.comp.loc[point: (point+2), "Sample"] = compon.getName()
         self.comp.loc[point: (point+2), "ug per well recPrP"] = compon.getConc()
         self.triblock += 1
-    def addData(self, file):
-        d = pd.read_csv(file)
-    def has_space(self):
-        return (self.triblock < 25)
+    def get_capacity(self):
+        return 25 - self.triblock
     def get_buff_vol(self):
-        return math.ceil((len(self.comp)*200*3*1.1)/1000)
-    def getComps(self):
+        return math.ceil((self.triblock*200*3*1.1)/1000)
+    def getComps(self): ##get plate components
         return self.comp
     def get_ab_dil(self, factor):
         printOut = (str(self.get_buff_vol() * (1000/factor))+ " ul Ab in " + \
@@ -64,26 +73,27 @@ str(self.get_buff_vol()) + "mls")
         printOut += self.comp.__str__()
         return printOut
 
-class Plate_calib(Plate):
-    def __init__(self, name):
-        Plate.__init__(self, name)
-        self.comp = {"11BD":"HaRecPrP 1 Denat",
-                     "11EG":"HaRecPrP 0.75 Denat",
-                     "12BD":"HaRecPrP 0.5 Denat",
-                     "12EG":"HaRecPrP 0.25 Denat"}
-        print(self.comp)
-    def has_space(self):
-        return (self.triblock < 21)
-
 class Tissue(Material):
     def __init__(self, name, dis_state, organ):
         Material.__init__(self, name)
         self.dis_state = dis_state
         self.organ = organ
+        self.PKtreat = 0.0
     def getDis_state(self):
         return self.dis_state
+    def setPKtreat(self, val):
+        self.PKtreat = val
+    def getPKtreat(self):
+        return self.PKtreat
     def getOrgan (self):
         return self.organ
+
+class Calib(Material):
+    def __init__(self, name, conc):
+        Material.__init__(self, name)
+        self.conc = conc
+    def getConc(self):
+        return self.conc
 
 class Experiment(Material):
     def __init__(self, name, start_date, numReps):
@@ -92,16 +102,18 @@ class Experiment(Material):
         self.numReps = numReps
 
 class CDI(Experiment):
-    def __init__(self, name, start_date, numReps, tissues, PK_concs):
+    def __init__(self, name, start_date, numReps, tissues,
+                 PK_concs, calib_name, calib_concs):
         Experiment.__init__(self, name, start_date, numReps)
         self.tissues = tissues
         self.PK_concs = PK_concs
+        self.calib_name = calib_name
+        self.calib_concs = calib_concs
         self.tiss_vol = self.numReps * len(PK_concs) * 60
-        self.samples = []
-        self.plates = []
+        self.samples , self.plates, self.calibs = [],[],[]
     def print_PKprep(self):
         printOut = "To prepare PK dils, for adding 1.5ul per 60ul brain homog:\n"
-        for c in PK_concs:
+        for c in self.PK_concs:
             PK_vol = 50
             printOut += \
 str((1-(c/50))*PK_vol)+"ul water plus " + \
@@ -117,60 +129,34 @@ str((c/50)*PK_vol) + "ul 2mg/ml PK\n"
         for r in range(self.numReps):
             for t in self.tissues:
                 for c in self.PK_concs:
-                    sample = t.getName()+ ": " + str(c)
-                    self.samples.append(sample + "Native")
-                    self.samples.append(sample + "Denatured")
-    def fillPlates(self, comp_list):
+                    self.samples.append((t,c))
+    def set_calibs(self):
+        for i in self.calib_concs:
+            c = Calib(self.calib_name, i)
+            self.calibs.append(c)
+    def fillPlatesCalib(self):
+        self.set_samples()
+        self.set_calibs()
         plate_num = 1
         p = Plate(plate_num)
-        for c in comp_list:
-            if p.has_space():
-                p.addAnalyte(c)
+        for t,c in self.samples:
+            if p.get_capacity() > 5:
+                p.addAnalyte((t,c))
             else:
+                for c in self.calibs:
+                    p.addCalibrant(c)
                 self.plates.append(p)
                 plate_num += 1
                 p = Plate(plate_num)
-                p.addAnalyte(c)
+##                p.addAnalyte((t,c))
         self.plates.append(p)
-        
-    def fillPlatesTissues(self):
-        self.set_samples()
-        self.fillPlates(self.samples)
     def __str__(self):
         printOut = "Plates:\n"
         printOut += "Tissue vols required: "+str(self.get_tiss_vol()) +"\n"
         for p in self.plates:
             printOut += p.__str__()
         return printOut
-
-class CDI_calib(CDI):
-    def fillPlates(self, comp_list):  
-        plate_num = 1
-        p = Plate_calib(plate_num)
-        for c in comp_list:
-            if p.has_space():
-                p.addAnalyte(c)
-            else:
-                self.plates.append(p)
-                plate_num += 1
-                p = Plate_calib(plate_num)
-                p.addAnalyte(c)
     
-for key, value in FFI.items():
-    tissues.append(Tissue(key, "FFI", value))
-for key, value in fCJD.items():
-    tissues.append(Tissue(key, "fCJD", value))
-for key, value in sFI.items():
-    tissues.append(Tissue(key, "sFI", value))
-
-
-class Calib(Material):
-    def __init__(self, name, conc):
-        Material.__init__(self, name)
-        self.conc = conc
-    def getConc(self):
-        return self.conc
-
 def get_cal_dils(subname, subconc, cal_ug_vals, stock_dilFac, 
                  numCurves = 6, sample_vol = 25):
     cal_vol = sample_vol * (numCurves + 1)
@@ -192,51 +178,34 @@ def get_cal_dils(subname, subconc, cal_ug_vals, stock_dilFac,
         print(printOut)
     return cal_vals, buf_vals
 
+for key, value in FFI.items():
+    tissues.append(Tissue(key, "FFI", value))
+for key, value in fCJD.items():
+    tissues.append(Tissue(key, "fCJD", value))
+for key, value in sFI.items():
+    tissues.append(Tissue(key, "sFI", value))
+
 subconc = {"431":0.2515, "432":0.2253333}
 cal_ug_vals = [0.25, 0.50, 0.75, 1.00]
-
 
 ####get_cal_dils("old HuRePrP", 0.691, cal_ug_vals, 0.1, numCurves = 2)
 ##get_cal_dils("431", 0.2515, concs, 1, numCurves = 3)
 ##get_cal_dils("432", 0.2253333, concs, 1, numCurves = 3)
 
+##calib_test = []
+##reps = 2
+##subs = "431" , "432"
+##concs = [0.0, 0.25, 0.50, 0.75, 1.00]
+##for r in range(reps):
+##    for s in subs:
+##        for c in concs:
+##            calib_test.append(Calib(s,c))    
+##e = CDI("1", "12-3-21", 2, tissues, PK_concs)
+##e.fillPlates(calib_test)
+##p = e.getPlates()[0]
+##p_df = p.getComps()
+##print(p_df.iloc[0:45,:])
 
-calib_test = []
-reps = 2
-subs = "431" , "432"
-concs = [0.0, 0.25, 0.50, 0.75, 1.00]
-for r in range(reps):
-    for s in subs:
-        for c in concs:
-            calib_test.append(Calib(s,c))    
-e = CDI("1", "12-3-21", 2, tissues, PK_concs)
-e.fillPlates(calib_test)
-
-p = e.getPlates()[0]
-p_df = p.getComps()
-print(p_df.iloc[0:45,:])
-
-d = pd.read_csv("CDI_21_001_column.csv")
-print(d)
-s = d.iloc[9:,4]
-s = s.reset_index(drop = True)
-s = pd.to_numeric(s)
-print(s[:8])
-p_df["CDI vals"]= s
-print(p_df.iloc[:20,:])
-
-def calib_plot(df , sub):
-    df_sub = df[df["Sample"]==sub]
-    polynomial_coeff=np.polyfit(df_sub["ug per well recPrP"], df_sub["CDI vals"],1)
-    print(polynomial_coeff)
-    plt.scatter(x=df_sub["ug per well recPrP"], y=df_sub["CDI vals"])
-    plt.xlabel("ug per well recPrP")
-    plt.ylabel("CDI values")
-    plt.title("HuRecPrP " + sub + "; Slope: "+ str(polynomial_coeff.round()[0]))
-    xnew=np.linspace(0,1,100)
-    ynew=np.poly1d(polynomial_coeff)
-    plt.plot(xnew,ynew(xnew),'r')
-    plt.show()
-
-calib_plot(p_df , "432")
-
+e =CDI(1, "", 2, tissues, PK_concs, "431", cal_ug_vals)
+e.fillPlatesCalib()
+print(e)
